8690f1e701878f0668d649ec8dd1abce13bdd150
Stephen L Arnold <nerdboy@gentoo.org>
2021-08-25 00:38:06 -0700

    chg: dev: add .gitattributes, try and fix windows build
    Signed-off-by: Stephen L Arnold <nerdboy@gentoo.org>

--- conda/bld.bat
+++ conda/bld.bat
@@ -1,4 +1,6 @@
-ECHO "hiredis library"
+@rem bld.bat for hiredis library
+setlocal EnableDelayedExpansion
+echo on
 
 mkdir _build
 cd _build
@@ -7,7 +9,7 @@ env
 REM add future installation path to pkgconfig
 set PKG_CONFIG_PATH=%LIBRARY_PREFIX%\lib\pkgconfig;
 
-cmake -G"NMake Makefiles" ^
+cmake -G"Ninja" ^
       -DCMAKE_BUILD_TYPE=Release ^
       -DDISABLE_TESTS:BOOL=ON ^
       -DCMAKE_INSTALL_PREFIX:PATH=%LIBRARY_PREFIX% ^
@@ -15,11 +17,13 @@ cmake -G"NMake Makefiles" ^
       ..
 if errorlevel 1 exit 1
 
-nmake VERBOSE=1
+REM nmake VERBOSE=1
+cmake --build . --config Release
+if errorlevel 1 exit 1
+cmake --install . --config Release
+REM nmake install VERBOSE=1
+
 if errorlevel 1 exit 1
 
-nmake install VERBOSE=1
-if errorlevel 1 exit 1
- 
 cd ..
 
